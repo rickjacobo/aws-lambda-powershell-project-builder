98ff937acdcdc36d9be76dc360dd96574a12f94d
rickjacobo <rick@rickjacobo.com>
2021-01-10 13:31:45 -0800

    .

--- create-project.ps1
+++ create-project.ps1
@@ -6,11 +6,13 @@ $Memory = Read-Host "Enter Desired Memory Allocation ie, 198"
 $Dir = "./" +  "$Project"
 $FileName = "$Project" + ".ps1"
 $FilePath = "$Dir/$FileName"
+$PublishName = "$Project" + "-" + "publish.ps1"
+$EditName = "$Project" + "-" + "edit.ps1"
 
 New-AWSPowerShellLambda -ScriptName $Project -Template Basic
 
-Write-Output "Publish-AWSPowerShellLambda -ScriptPath $FilePath -Name $Project -Region $Region -Memory $Memory" | Out-File publish.ps1
+Write-Output "Publish-AWSPowerShellLambda -ScriptPath $FilePath -Name $Project -Region $Region -Memory $Memory" | Out-File $PublishName
 
-Write-Output "vi $FilePath" | Out-File edit.ps1
+Write-Output "vi $FilePath" | Out-File $EditName
 
 aws configure
